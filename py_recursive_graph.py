0eaefcdc45889b522a3b0c05167327167ccc199f
Ernesto <44853461+ernestoherrerab@users.noreply.github.com>
2022-03-11 14:18:51 +0100

    Support for 4 levels

--- py_recursive_graph.py
+++ py_recursive_graph.py
@@ -67,7 +67,8 @@ def rebuild_inventory(results, input_dict, nornir_session):
                     device_ip = list(input_dict[site_id][host]["show_cdp_neighbors_detail"]["index"][index]["entry_addresses"].keys())
                 if "interface_addresses" in input_dict[site_id][host]["show_cdp_neighbors_detail"]["index"][index]:
                     device_ip = list(input_dict[site_id][host]["show_cdp_neighbors_detail"]["index"][index]["interface_addresses"].keys())
-                device_ip = device_ip[0]
+                if device_ip:
+                    device_ip = device_ip[0]
                 output_dict[device_id] = {}
                 output_dict[device_id]["hostname"] = device_ip
                 if "NX-OS" in input_dict[site_id][host]["show_cdp_neighbors_detail"]["index"][index]["software_version"]:
@@ -75,17 +76,18 @@ def rebuild_inventory(results, input_dict, nornir_session):
                 else:
                     output_dict[device_id]["groups"] = ["ios_devices"]
     for key, _ in output_dict.copy().items():
-        ip_address = ipaddress.IPv4Address(output_dict[key]["hostname"])
-        if ip_address.is_global:
+        if output_dict[key]["hostname"] != []:
+            ip_address = ipaddress.IPv4Address(output_dict[key]["hostname"])
+            if ip_address.is_global:
+                output_dict.pop(key, None)
+        else:
             output_dict.pop(key, None)
     return output_dict
 
 def main():
     ### PROGRAM VARIABLES ###
-    #username = input("Username: ")
-    #password = getpass(prompt="Password: ", stream=None)
-    username = "adminehb"
-    password = config("PASSWORD")
+    username = input("Username: ")
+    password = getpass(prompt="Password: ", stream=None)
     tmp_dict_output = {} 
     inv_path_file = Path("inventory/") / "hosts.yml"   
     diagrams_path = Path("diagrams/")
@@ -95,6 +97,7 @@ def main():
     """
     Fetch sent command data, format results, and put them in a dictionary variable
     """
+    ### FIRST LEVEL ###
     print("Initializing connections to devices...")
     nr, results = init_nornir(username, password)
 
@@ -105,46 +108,64 @@ def main():
     ### REBUILD INVENTORY FILE BASED ON THE NEIGHBOR OUTPUT ###    
     host_dict = rebuild_inventory(results, tmp_dict_output, nr)
     host_yaml = dump(host_dict, default_flow_style=False)
+    print("Rebuild FIRST Inventory ...")
     with open(inv_path_file, "a") as open_file:
         open_file.write("\n" + host_yaml)
 
-    print("Initializing connections to devices in new inventory file...")
+    ### SECOND LEVEL ###
+    print("Initializing connections to devices in SECOND inventory file...")
     nr, results = init_nornir(username, password)
 
     ### CREATE SITE ID DICTIONARIES ###
     print("Parsing generated output...")
-    ### CREATE SITE ID DICTIONARIES ###
     tmp_dict_output = build_sites(results, nr)
-    rec_host_dict = rebuild_inventory(results, tmp_dict_output, nr)
+    sec_host_dict = rebuild_inventory(results, tmp_dict_output, nr)
 
     ### MERGE YAML OBJECTS TO UPDATE INVENTORY FILE ###
-    print("Rebuild Inventory ...")
+    print("Rebuild SECOND Inventory ...")
     with open(inv_path_file) as f:
         inv_dict = load(f, Loader=FullLoader )
-    inv_final = {**rec_host_dict, **inv_dict}
-    yaml_inv = dump(inv_final, default_flow_style=False)
+    inv_tmp = {**sec_host_dict, **inv_dict}
+    yaml_inv = dump(inv_tmp, default_flow_style=False)
     with open(inv_path_file, "w+") as open_file:
         open_file.write("\n" + yaml_inv)
 
-    print("Initializing connections to devices in new inventory file...")
+    ### THIRD LEVEL ###
+    print("Initializing connections to devices in THIRD inventory file...")
     nr, results = init_nornir(username, password)  
     
     ### CREATE SITE ID DICTIONARIES ###
+    print("Parsing generated output...")
     tmp_dict_output = build_sites(results, nr)
+    third_host_dict = rebuild_inventory(results, tmp_dict_output, nr)
+    print("Rebuild THIRD Inventory ...")
+    with open(inv_path_file) as f:
+        inv_dict = load(f, Loader=FullLoader )
+    inv_final = {**third_host_dict, **inv_dict}
+    yaml_inv = dump(inv_final, default_flow_style=False)
+    with open(inv_path_file, "w+") as open_file:
+        open_file.write("\n" + yaml_inv)
+    
+    print("Initializing connections to devices in FINAL inventory file...")
+    nr, results = init_nornir(username, password)      
+    ### MERGE YAML OBJECTS TO UPDATE INVENTORY FILE ###
+    print("Parse data from FINAL Inventory")
+    inv_dict_output = build_sites(results, nr)
     for result in results.keys():
         host = str(nr.inventory.hosts[result])
         site_id = host.split("-")
         site_id = site_id[0]
-        tmp_dict_output[site_id][host] = {}
-        tmp_dict_output[site_id][host] = dict(nr.inventory.hosts[result])
+        inv_dict_output[site_id][host] = {}
+        inv_dict_output[site_id][host] = dict(nr.inventory.hosts[result])
     ### CREATE TUPPLES LIST ###
-    for site in tmp_dict_output:
+    print("Generating Graph Data...")
+    for site in inv_dict_output:
         cdp_tuple_list = []  
-        for host in tmp_dict_output[site]:
+        for host in inv_dict_output[site]:
             neighbor_tuple = ()
-            if tmp_dict_output[site][host] != {}:
-                for index in tmp_dict_output[site][host]["show_cdp_neighbors_detail"]["index"]:
-                    neighbor = tmp_dict_output[site][host]["show_cdp_neighbors_detail"]["index"][index]["device_id"].split(".")
+            if inv_dict_output[site][host] != {}:
+                for index in inv_dict_output[site][host]["show_cdp_neighbors_detail"]["index"]:
+                    neighbor = inv_dict_output[site][host]["show_cdp_neighbors_detail"]["index"][index]["device_id"].split(".")
                     neighbor = neighbor[0]
                     hostname = host.split("(")
                     hostname = hostname[0]
@@ -152,7 +173,6 @@ def main():
                     cdp_tuple_list.append(neighbor_tuple)
         if cdp_tuple_list:
             cdp_tuples_list.append(cdp_tuple_list)
-
     """    
     Generate Graph
     """
